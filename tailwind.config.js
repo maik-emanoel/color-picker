2e28cf62b58624ac3056cc1b6d555e84e19f63d7
Maik Emanoel <maikemanoel11@gmail.com>
2023-06-17 14:07:40 -0300

    set some default config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hueGradient': "linear-gradient(90deg, #FF0000 5.21%, #FF8A00 16.48%, #FFE600 27.74%, #14FF00 39.35%, #00A3FF 49.37%, #0500FF 61.18%, #AD00FF 72.26%, #FF00C7 83.53%, #FF0000 94.61%);"
+      }
+    },
   },
   plugins: [],
 }
